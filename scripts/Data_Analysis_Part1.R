8338c0cef4d14fc3eec0b92c91e26633c4602b9a
mandarpriya <mandarpriya@gmail.com>
2025-03-07 21:06:21 +0100

    Create Data_Analysis_Part1.R
    data analysis using Temperature data

---
+++ scripts/Data_Analysis_Part1.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(SPEI)
+
+## reading the monthly data from the folder
+monthly_tbl <- readRDS("data/monthly_tbl.rds")
+
+# So now we can start   our data analysis 
+
+## Here we focus on Temperature 
+
