80feb787339696f43313b1b3736bc11f7e0ba4bf
mandarpriya <mandarpriya@gmail.com>
2025-03-08 22:06:40 +0100

    script updating

--- scripts/Data_Analysis_Part1.R
+++ scripts/Data_Analysis_Part1.R
@@ -1,10 +1,43 @@
 library(tidyverse)
 library(SPEI)
+library(ggplot2)
+
 
 ## reading the monthly data from the folder
 monthly_tbl <- readRDS("data/monthly_tbl.rds")
+monthly_tbl <- monthly_tbl |> mutate(date = ymd(date)) |> drop_na()
 
-# So now we can start   our data analysis 
+monthly_tbl |> 
+  ggplot(aes(date, TAVG)) +
+  geom_line(color = "#FDA666",linewidth  = 0.5) +
+  scale_x_date(breaks = c(seq(as.Date("1891-01-01"), as.Date("2021-01-01"), by = "10 years"), 
+             as.Date("2025-01-01"))) +
+  scale_y_continuous(
+    breaks = seq(-10, 25, by = 5),
+    labels = function(x) paste0(format(x, nsmall = 0), " °C")
+  ) + 
+  coord_cartesian(xlim = c(as.Date("1891-01-01"), as.Date("2025-01-01")),
+                  ylim = c(-10, 25)) +
+  theme_minimal() +
+theme(
+plot.title = element_text(hjust = 0),
+plot.title.position = "plot",
+plot.subtitle = element_text(hjust = 0),
+axis.text.x = element_text(angle = 45, hjust = 1, color = "#3D4551"),
+axis.text.y = element_text( color = "#3D4551"),
+axis.title = element_text( color = "#3D4551" ),
+axis.line = element_line(color = "#7393B3", linewidth  = 1),
+panel.background = element_blank(),
+plot.background = element_blank(),
+panel.grid.minor = element_blank(),
+panel.grid.major = element_line(color = "lightgrey", linewidth  = 0.5),
+axis.line.y.right = element_line(color = "#7393B3", linewidth = 0.5),
+axis.text.y.right = element_text(color = "#3D4551"),
+axis.title.y.right = element_text( angle = 90)) +
+  labs(title = "Hamburg Fuhlsbüttel Average Temperature",
+       subtitle = "Period:-1891-2024",
+        x = "", 
+       y = "Average Temperature",) 
+  
 
-## Here we focus on Temperature 
 
